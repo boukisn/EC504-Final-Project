fdc0aaa656601e660d995647c9a85f71ac1a8d3a
Nick Boukis <poisongreekgyro@gmail.com>
2016-04-11 22:39:28 -0400

    Added missing db1.py file

---
+++ db1/db1.py
@@ -0,0 +1,4 @@
+from dataExchange import *
+
+clientSync(filename='DC1-sampleQueries.txt', ip_to='169.254.226.186', ip_from='169.254.106.129')
+clientSync(filename='client_out.txt', ip_to='127.0.0.1', ip_from='127.0.0.1')
